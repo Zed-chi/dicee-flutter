4fb87bb8546d319f507caa6b7e774d682fd4a108
zed <zed@mail.ru>
2019-12-04 01:48:39 +0300

    added random updating of dices

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "dart:math";
 
 void main() {
   return runApp(
@@ -15,9 +16,50 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+
+class DicePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void changeNumber(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6)+1;
+      rightDiceNumber = Random().nextInt(6)+1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {    
+    return Center(
+      child: Row(
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: FlatButton(
+            onPressed: changeNumber,
+            padding: EdgeInsets.all(0),
+            child: Image(
+              width: 200,
+              image: AssetImage("images/dice$leftDiceNumber.png"),
+            ),
+          ),
+        ),
+        Expanded(
+          flex:1,
+          child: FlatButton(
+            padding: EdgeInsets.all(0),
+            onPressed: changeNumber,
+            child: Image(
+            width: 200,
+            image: AssetImage("images/dice$rightDiceNumber.png"),
+          ),
+          )
+        )        
+      ],
+    ),
+    );
   }
 }
